dfb4a73f0784b1c64ff5a4d111bd6710520c8100
Unknown <tom.watt@protonmail.com>
2018-08-26 15:09:56 +0800

    Fixing wordlist size

--- wordlist.py
+++ wordlist.py
@@ -41,7 +41,7 @@ def filter_list(word_list, list_size):
 
     """
     result = set()
-    while len(result) <= list_size:
+    while len(result) < list_size:
         word = random.choice(word_list)
         if word[0].islower() and word.isalpha() and word not in result:
             result.add(word)

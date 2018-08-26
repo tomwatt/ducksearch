474408c25d484dec07fced84fb6455a0bfba4930
Unknown <tom.watt@protonmail.com>
2018-08-26 17:38:48 +0800

    Importing request get mock

--- test_wordlist.py
+++ test_wordlist.py
@@ -1,23 +1,7 @@
 """Unit tests for wordlist module."""
 from unittest import TestCase, mock, main
 import wordlist
-
-
-def mocked_requests_get(*args):
-    """Will be used by the mock to replace requests.get."""
-    class MockResponse:
-        def __init__(self, text, status_code):
-            self.text = text
-            self.status_code = status_code
-
-    if args[0] == wordlist.WORD_LIST_URL:
-        with open('test_list.txt', 'r') as words_file:
-            data = words_file.read()
-        return MockResponse(data, 200)
-
-
-    return MockResponse(None, 404)
-
+from request_mock import mocked_requests_get
 
 class TestWordlist(TestCase):
     """Wordlist unit test suite."""
